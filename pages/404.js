8f00359884e70c656f8d879a333e1a496e5ac5d2
yuxxeun <itsyuxxeun@gmail.com>
2022-03-19 02:55:49 +0700

    fix: 404 narasi

--- pages/404.js
+++ pages/404.js
@@ -6,7 +6,7 @@ const Ilegal = () => {
           451 ㅡ Unavailable For Legal Reasons
         </h1>
         <p className="text-lg pt-5 italic">
-	  Why show a generic 404 when I can make it sound mysterious? It seems you've found something that used to exist, or you spelled something wrong. I'm guessing you spelled something wrong. Can you double check that URL?
+	  Why show a generic 404 when I can make it sound mysterious? It seems you&apos;ve found something that used to exist, or you spelled something wrong. I&apos;m guessing you spelled something wrong. Can you double check that URL?
         </p>
       </div>
     </>
