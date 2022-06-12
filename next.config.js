ad388ef9b408ba7af04c66f35fa72b72e1b24d27
yuxxeun <itsyuxxeun@gmail.com>
2022-06-12 21:52:54 +0700

    conf: enabled experimentals features

--- next.config.js
+++ next.config.js
@@ -3,13 +3,20 @@ const withPWA = require("next-pwa");
 const nextConfig = withPWA({
   reactStrictMode: true,
   swcMinify: true,
-  concurrentFeatures: true,
   pwa: {
     dest: "public",
     register: true,
     skipWaiting: true,
     scope: "/",
   },
+
+  // enabled experimentals features
+  experimentals: {
+    concurrentFeatures: true,
+    serverComponents: true
+  },
+
+  // redirect to random URL
   async redirects() {
     return [
       {
