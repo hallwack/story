2f3cf07fe5c70bf69fc8f797fff43fa5ca601a7d
yuxxeun <itsyuxxeun@gmail.com>
2022-06-24 21:28:50 +0700

    feat: aos animation

--- components/Avatar.jsx
+++ components/Avatar.jsx
@@ -5,7 +5,15 @@ const name = "krido pambudi";
 export default function Avatar() {
   return (
     <>
-      <div className="xs:w-2/3 mx-auto mt-5 w-5/6 items-center pb-5 text-center focus:outline-none">
+      <div className="xs:w-2/3 mx-auto mt-5 w-5/6 items-center pb-5 text-center focus:outline-none"
+	    data-aos="fade-up"
+    data-aos-offset="200"
+    data-aos-delay="50"
+    data-aos-duration="1000"
+    data-aos-easing="ease-in-out"
+    data-aos-mirror="true"
+    data-aos-once="false"
+    data-aos-anchor-placement="top-center">
         <div>
           <Image
             className="rounded-full"
